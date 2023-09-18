618168ef26116a58c8febbab94beb660d8df326f
kk17203 <kk17203@gmail.com>
2023-09-18 06:44:41 -0500

    restructure project

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "../styles/GeneralInfo.css";
+
+function GeneralInfo() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h1>Starting Over</h1>
+        </>
+    );
+}
+
+export default GeneralInfo;
