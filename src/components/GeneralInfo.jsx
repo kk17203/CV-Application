66ffc326044feb3f615cfa5bf54126f8d1fc6f89
kk17203 <kk17203@gmail.com>
2023-09-18 13:01:15 -0500

    Added 'General Info' component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -2,12 +2,60 @@ import { useState } from "react";
 import "../styles/GeneralInfo.css";
 
 function GeneralInfo() {
-    const [count, setCount] = useState(0);
+    const [submit, setSubmit] = useState(false);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
 
+    if (submit) {
+        return (
+            <div className="general-info">
+                <h1>General Info</h1>
+                <p>{fullName}</p>
+                <p>{email}</p>
+                <p>{phone}</p>
+                <button
+                    onClick={() => {
+                        setSubmit(false);
+                        setFullName(fullName);
+                        setEmail(email);
+                        setPhone(phone);
+                    }}
+                >
+                    Edit
+                </button>
+            </div>
+        );
+    }
     return (
-        <>
-            <h1>Starting Over</h1>
-        </>
+        <div className="general-info">
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    setSubmit(true);
+                }}
+            >
+                <input
+                    type="text"
+                    placeholder="Full Name"
+                    onChange={(e) => setFullName(e.target.value)}
+                    value={fullName}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <input
+                    type="tel"
+                    placeholder="Phone Number"
+                    onChange={(e) => setPhone(e.target.value)}
+                    value={phone}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     );
 }
 
